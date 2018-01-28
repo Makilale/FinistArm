c2f8b39474328a2a3714771b2434ba01d3502e72
alex <ma3ca3@gmail.com>
2018-01-28 18:21:41 +0500

    #tets2

--- app/src/main/java/com/corp/a/i/finistarm/Login.java
+++ app/src/main/java/com/corp/a/i/finistarm/Login.java
@@ -1,7 +1,9 @@
 package com.corp.a.i.finistarm;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
