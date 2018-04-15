e1321c57407c86079734fe6ad90c864df1d589d6
IlyasKunakaev <embress01@bk.ru>
2018-04-16 01:59:13 +0500

    Some Design

--- app/src/main/java/com/corp/a/i/finistarm/ChoseMenu.java
+++ app/src/main/java/com/corp/a/i/finistarm/ChoseMenu.java
@@ -2,12 +2,28 @@ package com.corp.a.i.finistarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ChoseMenu extends AppCompatActivity {
 
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chose_menu);
+        btnBack = (Button) findViewById((R.id.btnBackTable));
+        View.OnClickListener oclBtnBack = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        };
+
+        btnBack.setOnClickListener(oclBtnBack);
+
+
     }
+
+
 }
